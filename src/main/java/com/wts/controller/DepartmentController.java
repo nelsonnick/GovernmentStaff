b0084f520d1e2f0673efba9e03ad9adb2618cf75
nelsonnick <nelsonnick@126.com>
2019-01-08 08:15:48 +0800

    .

--- src/main/java/com/wts/controller/DepartmentController.java
+++ src/main/java/com/wts/controller/DepartmentController.java
@@ -71,4 +71,60 @@ public class DepartmentController extends Controller {
     public void getDWLB(){
         renderJson(Db.find("SELECT DISTINCT dwlb FROM department WHERE szcs= '" + getPara("szcs") + "' AND dwzd = '" + getPara("dwzd") + "' "  ));
     }
+
+    public void g(){
+        String str ="[";
+        List<Record> szcs = Db.find("SELECT DISTINCT szcs FROM department" );
+        for (Record s :szcs){
+            str = str +"{'label':'"+s.getStr("szcs")+"','value':'"+s.getStr("szcs")+"','children': [";
+            List<Record> dwzd = Db.find("SELECT DISTINCT dwzd FROM department WHERE szcs= '" + s.getStr("szcs") + "' " );
+            for (Record d :dwzd){
+                str = str +"{'label':'"+d.getStr("dwzd")+"','value':'"+d.getStr("dwzd")+"','children': [";
+                List<Record> dwlb =Db.find("SELECT DISTINCT dwlb FROM department WHERE szcs= '" + s.getStr("szcs") + "' AND dwzd = '" + d.getStr("dwzd") + "' "  );
+                for (Record b :dwlb){
+//                    str = str +"{'label':'"+b.getStr("dwlb")+"','value':'"+b.getStr("dwlb")+"'},";
+                    str = str +"{'label':'"+b.getStr("dwlb")+"','value':'"+b.getStr("dwlb")+"','children': [";
+                    List<Record> dwlx =Db.find("SELECT DISTINCT dwlx FROM department WHERE szcs= '" + s.getStr("szcs") + "' AND dwzd = '" + d.getStr("dwzd") + "' AND dwlb = '" + b.getStr("dwlb") + "' " );
+                    System.out.println("SELECT DISTINCT dwlx FROM department WHERE szcs= '" + s.getStr("szcs") + "' AND dwzd = '" + d.getStr("dwzd") + "' AND dwlb = '" + b.getStr("dwlb") + "' " );
+                    for (Record x :dwlx){
+                        str = str +"{'label':'"+x.getStr("dwlx")+"','value':'"+x.getStr("dwlx")+"'},";
+                    }
+                    str = str.substring(0,str.length()-1);
+                    str = str + "]},";
+                }
+                str = str.substring(0,str.length()-1);
+                str = str + "]},";
+            }
+            str = str.substring(0,str.length()-1);
+            str = str + "]},";
+        }
+        str = str.substring(0,str.length()-1);
+        str = str + "]";
+        String s = str.replace("'党委'", "a").replace("'人大'", "b").replace("'政府'", "c")
+                .replace("'政协'", "d").replace("'民主党派'", "e").replace("'群众团体'", "f")
+                .replace("'法院'", "g").replace("'检察院'", "h").replace("'经济实体'", "i")
+                .replace("'街道办事处'", "j").replace("'乡'", "j").replace("'镇'", "l");
+        System.out.println(str.replace("'","\"").replace(" ",""));
+        renderText(str.replace("'","\"").replace(" ",""));
+    }
+
+    public void gg(){
+        String str ="[";
+        List<Record> szcs = Db.find("SELECT DISTINCT szcs FROM department" );
+        for (Record s :szcs){
+            str = str +"{'label':'"+s.getStr("szcs")+"','children': [";
+            List<Record> dwzd = Db.find("SELECT DISTINCT dwzd FROM department WHERE szcs= '" + s.getStr("szcs") + "' " );
+            for (Record d :dwzd){
+                str = str +"{'label':'"+d.getStr("dwzd")+"','children': [";
+                List<Record> dwlb =Db.find("SELECT DISTINCT dwlb FROM department WHERE szcs= '" + s.getStr("szcs") + "' AND dwzd = '" + d.getStr("dwzd") + "' "  );
+                for (Record b :dwlb){
+                    str = str +"{'label':'"+b.getStr("dwlb")+"'},";
+                }
+                str = str + "],},";
+            }
+            str = str + "],},";
+        }
+        str = str + "]";
+        System.out.println(str);
+    }
 }
